f0afab5ce7694d1a027e37a29fc3731a22f8b267
Denys Rusov <rusovd@gmail.com>
2019-10-30 20:37:42 +0200

    Updates

---
+++ precache-manifest.fcc73e96ea86982a3b5a6a4c72ce743f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "235493532c30c36b255c76e797b42bde",
+    "url": "/garbgs-client/index.html"
+  },
+  {
+    "revision": "6479426cfb0b225445c8",
+    "url": "/garbgs-client/static/css/main.67393c3a.chunk.css"
+  },
+  {
+    "revision": "6e3159e51e8084472870",
+    "url": "/garbgs-client/static/js/2.2c8b2d1e.chunk.js"
+  },
+  {
+    "revision": "6479426cfb0b225445c8",
+    "url": "/garbgs-client/static/js/main.8a1ae0b3.chunk.js"
+  },
+  {
+    "revision": "afdbc74b2830a35a2c18",
+    "url": "/garbgs-client/static/js/runtime~main.74b7cce8.js"
+  }
+]);
